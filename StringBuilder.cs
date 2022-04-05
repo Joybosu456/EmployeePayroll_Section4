dba41ff5ddb0f7c926a30ac8f89bb65baa701f4e
JOY <joybosu@gmail.com>
2022-04-05 20:26:37 +0530

    [Joy] add string builder program file

---
+++ StringBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollOpration
+{
+    internal class StringBuilder
+    {
+        public StringBuilder()
+        {
+        }
+
+        public static implicit operator System.Text.StringBuilder(StringBuilder v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
